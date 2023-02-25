e6cf4a95a4f1a87a9d5ee1fc0080c8e1ea62ae64
Olli Appelt <appeltolli@gmail.com>
2023-02-25 01:46:43 +0100

    ASB V0.3
    Added:
    Intro Scene
    Static between scene switch
    
    Changed:
    FadeOut animation 1 sec longer

---
+++ Assets/IntroTime.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Unity.VisualScripting.Member;
+
+public class IntroTime : MonoBehaviour
+{
+    public float timeRemaining = 18;
+
+    private void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
